c39df3e11ce32836898b792ba7407e5a14e28d16
Jacampos <Jacampos@VP000031>
2017-10-18 09:06:10 -0500

    Proyecto Inicial

---
+++ EmmaSol/Emmaweb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Emmaweb.Startup))]
+namespace Emmaweb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
